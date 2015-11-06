ce996534cbd1d5bb9aa7c7625d34d546b1aec6c4
legrand <oceane.legrand@etudiant.univ-lille1.fr>
2015-11-06 11:42:08 +0100

    first commit

---
+++ src/BankAccount.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author legrand
+ *
+ */
+public class BankAccount {
+	protected int cents;
+
+	/**
+	 * @return the cents
+	 */
+	public int getCents() {
+		return cents;
+	}
+
+	/**
+	 * @param cents the cents to set
+	 */
+	public void setCents(int cents) {
+		this.cents = cents;
+	}
+	
+}
---
+++ src/Inhabitant.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+
+/**
+ * @author legrand
+ *
+ */
+public class Inhabitant {
+	protected String name;
+	protected City city;
+	protected BankAccount bankaccount;
+	
+	
+	
+	/**
+	 * @param name
+	 * @param city
+	 * @param bankaccount
+	 */
+	public Inhabitant(String name, City city, BankAccount bankaccount) {
+		this.name = name;
+		this.city = city;
+		this.bankaccount = bankaccount;
+	}
+
+
+
+	/**
+	 * @return the city
+	 */
+	public City getCity() {
+		return city;
+	}
+
+
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+
+
+	/**
+	 * @return the bankaccount
+	 */
+	public BankAccount getBankaccount() {
+		return bankaccount;
+	}
+
+
+
+	/**
+	 * @param bankaccount the bankaccount to set
+	 */
+	public void setBankaccount(BankAccount bankaccount) {
+		this.bankaccount = bankaccount;
+	}
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	
+}
