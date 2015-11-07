b35bed266cb415f08e637b8ebf518b7b71493c65
Fahima Negmi <negmifahima11@gmail.com>
2015-11-07 14:05:20 +0100

    la classe Letter

---
+++ src/Letter.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @author negmi
+
+ *
+ */
+
+public abstract class Letter <T extends Content> implements Content{
+	
+	 
+	 
+	protected  T content;
+	protected Inhabitant sender ;	 
+	protected Inhabitant receiver ;
+	 
+	//The constructor
+	public Letter (Inhabitant sender ,Inhabitant receiver ,T cont ){
+
+		this.sender=sender;
+		this.receiver=receiver ;
+		this.content=cont;
+		
+		
+	} 
+	
+   // Methods 
+	public abstract void action() ;
+	
+	public abstract int getCost() ;
+	
+	public T getContent(){
+		return this.content ;
+		
+	}
+
+	 
+	public Inhabitant getSender() {
+		return sender;
+	}
+	 
+
+	public Inhabitant getReceiver() {
+		return receiver;
+	}
+
+	 // return the type of letter
+	public abstract String getType();
+	}
+	
+	
