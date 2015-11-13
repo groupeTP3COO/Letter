2f460ef9d98b91e58c1a5e9041e91af3d308a27f
Fahima Negmi <negmifahima11@gmail.com>
2015-11-13 21:53:12 +0100

    la class money

---
+++ src/content/Money.java
@@ -0,0 +1,18 @@
+package content;
+ 
+/**
+ * @author negmi
+ *
+ */
+public class Money implements Content{
+	
+	public int amount;
+	public Money (int amount) {
+		
+		 this.amount=amount;
+		
+	}
+	
+	
+
+}
