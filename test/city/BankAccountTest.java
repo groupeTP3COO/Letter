07d7fd1338dcc7716b80a5531448f50b8019b6c9
OceaneL <oceane.legrand@etudiant.univ-lille1.fr>
2015-11-19 18:50:40 +0100

    ajout classe test de bankAccount

---
+++ test/city/BankAccountTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package city;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author user
+ *
+ */
+public class BankAccountTest {
+	
+	protected BankAccount bankAccount;
+	
+	@Before
+	public void setup() {
+		this.bankAccount = new BankAccount(10);
+	}
+	
+	
+	@Test
+	public void testGetAmount() {
+		assertEquals(10, this.bankAccount.getAmount());
+	}
+
+	/**
+	 * Test method for {@link city.BankAccount#credit(int)}.
+	 */
+	@Test
+	public void testCredit() {
+		this.bankAccount.credit(2);
+		assertEquals(12, this.bankAccount.getAmount());
+	}
+
+	/**
+	 * Test method for {@link city.BankAccount#debit(int)}.
+	 */
+	@Test
+	public void testDebit() {
+		this.bankAccount.debit(2);
+		assertEquals(8, this.bankAccount.getAmount());
+	}
+
+}
